e47f0eb3ea9ef2099164f8ec70becac3cec4ae4c
roshan-42 <roshanacharya207@gmail.com>
2024-05-11 19:13:43 +0545

    logo added

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -5,6 +5,9 @@ import Link from "next/link";
 import { Menu } from "lucide-react";
 import { CiDeliveryTruck } from "react-icons/ci";
 import { GiEarthAmerica } from "react-icons/gi";
+import { FaFacebook } from "react-icons/fa";
+
+import { FaInstagramSquare } from "react-icons/fa";
 
 export default function Navbar() {
   const [state, setState] = React.useState(false);
@@ -35,7 +38,10 @@ export default function Navbar() {
 
         <div className="flex items-center gap-3">
           <p className="text-sm">Follow Us</p>
-          <p>icons</p>
+          <div className="flex gap-2">
+            <FaFacebook />
+            <FaInstagramSquare />
+          </div>
         </div>
       </div>
       <div>
